524c15a90477bdc91533444a9d5f35493f8e0b40
Dzakiamriz22 <dzakiamriz12@gmail.com>
2026-01-09 10:12:08 +0700

    add StudentClassHistory resource with CRUD pages and actions

---
+++ app/Filament/Resources/StudentClassHistoryResource/Pages/ListStudentClassHistories.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\StudentClassHistoryResource\Pages;
+
+use App\Filament\Resources\StudentClassHistoryResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListStudentClassHistories extends ListRecords
+{
+    protected static string $resource = StudentClassHistoryResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [];
+    }
+}
